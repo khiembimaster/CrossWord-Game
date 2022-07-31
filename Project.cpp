63378cf0dfc3e1413ae926b6b071963512eea0e6
khiembimaster <masterkhiembi@gmail.com>
2022-07-31 20:31:19 +0700

    add some comments

--- Project.cpp
+++ Project.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 
 struct Pos{
+    //fast initializing
     Pos(int height, int weight, string direction): w{weight},h{height},Dir{direction} {}
     int w = 0;
     int h = 0;
@@ -30,34 +31,43 @@ TrieNode* getNode(){
 
 void buildFailure(TrieNode *pRoot){
     pRoot->failureLink = pRoot;
+    //Breadth first order traversal
     queue<TrieNode*> que;
     //Build First layer 
     for(int i = 0; i < 26; i++){
         if(!pRoot->children[i])
             continue;
         que.push(pRoot->children[i]);
+        //The first layer TrieNode will only point to root 
         pRoot->children[i]->failureLink = pRoot;
     }
 
     //Build the rest
     while(que.size()){
+        //Get current node
         TrieNode* cursor = que.front();
         que.pop();
+        //For each children
         for(int i = 0; i < 26; i++){
             if(!cursor->children[i])
-                continue;
-            TrieNode* thisChild = cursor->children[i];
+                continue; // skip if this is not a children
+            
+            TrieNode* thisChild = cursor->children[i]; 
+
+            //find Longest common suffix
             TrieNode* suffix = cursor->failureLink;
             while(!(suffix->failureLink->children[i]) && suffix != pRoot)
                 suffix = suffix->failureLink;
             
+            //if this suffix has a child with the same key as thisChild, set thisChild's failureLink to that suffix's child
             if(suffix->children[i])
                 thisChild->failureLink = suffix->children[i];
-            else thisChild->failureLink = pRoot;
+            else thisChild->failureLink = pRoot; // there are no common suffix 
 
+            //Push thisChild to que for later layer traversing
             que.push(thisChild);
         }
-
+        // inherit outputs
         for(string iter : cursor->failureLink->outputs)
             cursor->outputs.push_back(iter);
     
@@ -78,52 +88,53 @@ struct Table{
                 inp >> table[i][j];
             }
         }
+        //create trie
         string word;
         this->pRoot = getNode();
-        //create trie
         while((inp >> word) && (word != "#")){
             TrieNode* cursor = pRoot;
-            this->wordsList[word].push_back(Pos(0,0,"NF"));
+            this->wordsList[word].push_back(Pos(0,0,"NF")); // set output default value as Not found - a placeholder
             for(int i = 0; i<word.size(); ++i){
-                int index = word[i] - 'A';
-                if(!cursor->children[index])
-                    cursor->children[index] = getNode();
-                cursor = cursor->children[index];
+                int index = word[i] - 'A';// get the appropriate index from ASCII code
+                if(!cursor->children[index])// empty node so create new one
+                    cursor->children[index] = getNode(); 
+                cursor = cursor->children[index]; //follow the next level
             }
-            cursor->outputs.push_back(word);
+            cursor->outputs.push_back(word); // mark that the current pattern ends at this node
         }
 
         inp.close();
 
-        buildFailure(this->pRoot);
+        buildFailure(this->pRoot); 
     }
 
     void search(){
         
+        //Search row by row
         string Direction = "LR";
         for(int h = 1; h <= this->H; ++h){
             TrieNode* parent = this->pRoot;
             for(int w = 1; w <= this->W; ++w){
-                if(parent->children[table[h][w]-'A']) {
+                if(parent->children[table[h][w]-'A']) { //There is a link, Follow it
                     parent = parent->children[table[h][w]-'A'];
-                    if(parent->outputs.size()){
+                    if(parent->outputs.size()){ // this node of Trie is an end of one or some patterns
                         for(string out : parent->outputs){
-                            if(wordsList[out].size() == 1) ++wordFound;
-                            wordsList[out].push_back(Pos(h,w-out.size()+1,Direction));
+                            if(wordsList[out].size() == 1) ++wordFound; //this is the first time we match this pattern, Increase word found.
+                            wordsList[out].push_back(Pos(h,w-out.size()+1,Direction));// Store the Position to wordList
                         }
                     }
                 }
-                else{
-                    while(parent != pRoot && !parent->children[table[h][w]-'A'])
-                        parent = parent->failureLink;
+                else{ //Link end/ fail/ unmatch
+                    while(parent != pRoot && !parent->children[table[h][w]-'A']) //find the other path with common suffix
+                        parent = parent->failureLink; 
                     
-                    if(parent->children[table[h][w]-'A'])
-                        parent = parent->children[table[h][w]-'A'];
+                    if(parent->children[table[h][w]-'A']) //There is a link
+                        parent = parent->children[table[h][w]-'A']; // re-direct the parent pointer to where the process can continue.
                 }
             }
         }
 
-
+        //Search column by cloumn (the same)
         Direction = "TD";
         for(int w = 1; w <= this->W; ++w){
             TrieNode* parent = this->pRoot;
@@ -163,16 +174,19 @@ public:
 int main(){
     Table myGame("input.txt");
     myGame.search();
+
+    //Out put
     ofstream out("output.txt");
     out << myGame.wordFound << endl;
+    // for each key-value pair int map wordsList
     for(auto it : myGame.wordsList){
-        out << it.first << " ";
-        if(it.second.size() == 1)
+        out << it.first << " "; // string pattern 
+        if(it.second.size() == 1) // Not found
             out << "(" << 0 << "," << 0 << ")" << " " << "NF" << "| " << endl;
-        else {
+        else { // Founded
             for(Pos pos : it.second){
-                if(pos.Dir == "NF") continue;
-                out << "(" << pos.h << "," << pos.w << ")" << " " << pos.Dir << "| ";
+                if(pos.Dir == "NF") continue; // Since I initialized all the components in wordsList with a placeholders - Pos(0,0,"NF"). Let skip this.  
+                out << "(" << pos.h << "," << pos.w << ")" << " " << pos.Dir << "| "; // write down Pos
             }
             out << endl;
         }
